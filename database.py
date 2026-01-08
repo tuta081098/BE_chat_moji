018f81a1e3046d8fb32b6de8b41f8f8ee1ec7d12
Tu TA <>
2026-01-08 10:12:53 +0700

    [TuTA] update code

--- database.py
+++ database.py
@@ -1,22 +1,28 @@
+import os
 import motor.motor_asyncio
 from beanie import init_beanie
+from dotenv import load_dotenv # Import thư viện đọc file .env
+
+# Import các models của bạn
 from models.users import User
 from models.chat import Conversation, Message
 from models.friends import FriendRequest
 
-# Thay đổi URL nếu bạn dùng MongoDB Atlas (Cloud)
-MONGO_URL = "mongodb://localhost:27017"
+# 1. Load biến môi trường từ file .env (chỉ có tác dụng khi chạy Local)
+load_dotenv()
+
+# 2. Lấy URL từ biến môi trường.
+# Nếu không tìm thấy (lỡ quên set), nó sẽ fallback về localhost để không crash app
+MONGO_URL = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 DB_NAME = "chat_moji_db"
 
 async def init_db():
-    # Tạo client kết nối bất đồng bộ
+    # Tạo client kết nối
     client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
 
-    # Chọn database
     database = client[DB_NAME]
 
-    # Khởi tạo Beanie với các Models đã định nghĩa
-    # Lúc này Beanie sẽ tự động kiểm tra và tạo Collection/Index nếu chưa có
+    # Khởi tạo Beanie
     await init_beanie(database=database, document_models=[
         User,
         Conversation,
